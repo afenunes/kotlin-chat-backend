73b3d3cb85a38126c3faa417925c6a69bdcc0e4c
André <afenunes@gmail.com>
2020-03-05 09:08:01 +0000

    First commit

---
+++ src/main/kotlin/com/afen/kotlinchatbackend/KotlinChatBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.afen.kotlinchatbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinChatBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinChatBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/afen/kotlinchatbackend/KotlinChatBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.afen.kotlinchatbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinChatBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
